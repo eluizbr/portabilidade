2c102960b1f4cd0e941fca414c0a63b9b6a02239
Emerson Luiz <eluizbr@gmail.com>
2015-08-06 16:18:09 -0300

    updates

--- port/urls.py
+++ port/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.index, name='index'),
     url(r'^contato/$', views.contato, name='contato'),
-    url(r'^asterisk/$', views.asterisk, name='asterisk'),
+    url(r'^pabx/$', views.asterisk, name='pabx'),
     url(r'^padrao/$', views.padrao, name='padrao'),
     url(r'^csp-retorno/$', views.csp_retorno, name='csp-retorno'),
     url(r'^csp-retorno-del/(?P<deletar>\d+)$', views.csp_retorno_del, name='csp-retorno-del'),
